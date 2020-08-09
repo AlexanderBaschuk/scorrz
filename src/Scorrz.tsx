9e8bea7557d2688f7eab1d9345d998bbac2bb1ec
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-08-09 15:16:00 +0300

    Move results logic from Scorrs to useResults hook

--- src/Scorrz.tsx
+++ src/Scorrz.tsx
@@ -1,55 +1,15 @@
-import React, { useCallback, useMemo } from "react";
-import {
-	adjudicatorsSelector,
-	allResultsSelector,
-	competitorsSelector,
-} from "./Scorrz.selectors";
-
 import { AdjudicatorTable } from "./AdjudicatorTable/AdjudicatorTable";
-import { CompetitorId } from "./model/types";
 import { FinalTable } from "./FinalTable/FinalTable";
-import { calculateGridScores } from "./Calculations/calculations";
+import React from "react";
+import { adjudicatorsSelector } from "./Scorrz.selectors";
+import { useResults } from "./useResults";
 import { useSelector } from "react-redux";
 
 export const Scorrz: React.FC = () => {
-	const competitors = useSelector(competitorsSelector);
 	const adjudicators = useSelector(adjudicatorsSelector);
 	const rounds = ["H", "L", "S"];
-	const results = useSelector(allResultsSelector);
 
-	const getCompetitor = useCallback(
-		(id: CompetitorId) => {
-			return competitors.filter((c) => c.id === id)[0];
-		},
-		[competitors],
-	);
-
-	const getAdjudicatorResults = useCallback(
-		(adjudicatorId) => {
-			const resultLines = results[adjudicatorId].resultLines;
-			const sumsAndGrids = calculateGridScores(resultLines);
-			return resultLines.map((r) => ({
-				id: r.competitorId,
-				name: getCompetitor(r.competitorId).name,
-				scores: r.score,
-				sum: sumsAndGrids.get(r.competitorId).sum,
-				gridScore: sumsAndGrids.get(r.competitorId).grid,
-			}));
-		},
-		[results, getCompetitor],
-	);
-
-	const finalResults = useMemo(
-		() =>
-			// TODO: calculate the results
-			competitors.map((c, i) => ({
-				place: i + 1,
-				id: c.id,
-				name: c.name,
-				gridSum: 300 - 10 * i,
-			})),
-		[competitors],
-	);
+	const { getAdjudicatorResults, finalResults } = useResults();
 
 	return (
 		<>
---
+++ src/useResults.ts
@@ -0,0 +1,50 @@
+import { allResultsSelector, competitorsSelector } from "./Scorrz.selectors";
+import { useCallback, useMemo } from "react";
+
+import { CompetitorId } from "./model/types";
+import { calculateGridScores } from "./Calculations/calculations";
+import { useSelector } from "react-redux";
+
+export const useResults = () => {
+	const competitors = useSelector(competitorsSelector);
+	const results = useSelector(allResultsSelector);
+
+	const getCompetitor = useCallback(
+		(id: CompetitorId) => {
+			return competitors.filter((c) => c.id === id)[0];
+		},
+		[competitors],
+	);
+
+	const getAdjudicatorResults = useCallback(
+		(adjudicatorId: number) => {
+			const resultLines = results[adjudicatorId].resultLines;
+			const sumsAndGrids = calculateGridScores(resultLines);
+			return resultLines.map((r) => ({
+				id: r.competitorId,
+				name: getCompetitor(r.competitorId).name,
+				scores: r.score,
+				sum: sumsAndGrids.get(r.competitorId).sum,
+				gridScore: sumsAndGrids.get(r.competitorId).grid,
+			}));
+		},
+		[results, getCompetitor],
+	);
+
+	const finalResults = useMemo(
+		() =>
+			// TODO: calculate the results
+			competitors.map((c, i) => ({
+				place: i + 1,
+				id: c.id,
+				name: c.name,
+				gridSum: 300 - 10 * i,
+			})),
+		[competitors],
+	);
+
+	return {
+		getAdjudicatorResults,
+		finalResults,
+	};
+};
