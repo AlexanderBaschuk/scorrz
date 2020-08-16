c57679ab85e5eed5740bf17b15f05538fa70d5ef
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-08-16 17:47:30 +0300

    Return state from reducer

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -11,17 +11,20 @@ import { testInitialState } from "./model/types";
 export const rootReducer = createReducer(testInitialState, {
 	[calculateAction.type]: (state) => {
 		[state.adjudicatorTables, state.finalTable] = calculateResultTables(state);
+		return state;
 	},
 	[toggleAdjudicator.type]: (state, action) => {
 		state.selectedAdjudicators[action.payload] = !state.selectedAdjudicators[
 			action.payload
 		];
 		[state.adjudicatorTables, state.finalTable] = calculateResultTables(state);
+		return state;
 	},
 	[toggleRound.type]: (state, action) => {
 		state.selectedRounds[action.payload] = !state.selectedRounds[
 			action.payload
 		];
 		[state.adjudicatorTables, state.finalTable] = calculateResultTables(state);
+		return state;
 	},
 });
