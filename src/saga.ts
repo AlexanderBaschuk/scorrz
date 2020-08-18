d4535423798304e4c6cc4134e6da18b94069c562
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-08-19 00:00:34 +0300

    Remove console logs

--- src/saga.ts
+++ src/saga.ts
@@ -4,10 +4,8 @@ import { initResults } from "./actions";
 import { testResultsDto } from "./testResultsDto";
 
 function* loadResults() {
-	console.log("Loading results");
 	yield delay(1000);
 	yield put(initResults(testResultsDto));
-	console.log("Loaded results");
 }
 
 export function* mainSaga() {
