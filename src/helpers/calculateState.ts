0053749f056118d23f6696eeb604c44eb2b01e1f
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-08-21 00:23:40 +0300

    Change calculations folder name

--- src/helpers/calculateState.ts
+++ src/helpers/calculateState.ts
@@ -10,7 +10,7 @@ import {
 	SumAndGrid,
 	calculateFinalResults,
 	calculateGridScores,
-} from "@/calculations/calculations";
+} from "@/calc/calculations";
 
 export const calculateResultTables = (
 	state: State,
