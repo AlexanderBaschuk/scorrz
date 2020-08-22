f61fff5cbf880e3c6428cb020d5c5f8571f47f10
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-08-22 16:23:22 +0300

    Remove returning state from reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -14,28 +14,23 @@ import { initialState } from "./types";
 export const reducer = createReducer(initialState, {
 	[initResultsRequest.type]: (state) => {
 		state.isLoading = true;
-		return state;
 	},
 	[initResultsSuccess.type]: (_, action) => {
-		const state = initStateFromDto(action.payload);
-		return state;
+		return initStateFromDto(action.payload);
 	},
 	[initResultsFailure.type]: (state, action) => {
 		state.isLoading = false;
 		state.errorMessage = action.payload;
-		return state;
 	},
 	[toggleAdjudicator.type]: (state, action) => {
 		state.selectedAdjudicators[action.payload] = !state.selectedAdjudicators[
 			action.payload
 		];
-		return state;
 	},
 	[toggleRound.type]: (state, action) => {
 		state.selectedRounds[action.payload] = !state.selectedRounds[
 			action.payload
 		];
-		return state;
 	},
 	[toggleCompetitor.type]: (state, action) => {
 		const competitorIndex = state.selectedCompetitors.findIndex(
@@ -43,15 +38,12 @@ export const reducer = createReducer(initialState, {
 		);
 		if (competitorIndex >= 0) {
 			state.selectedCompetitors[competitorIndex] = null;
-			return state;
+		} else {
+			const firstEmptyIndex = state.selectedCompetitors.findIndex(
+				(value) => value === null,
+			);
+			if (firstEmptyIndex >= 0)
+				state.selectedCompetitors[firstEmptyIndex] = action.payload;
 		}
-
-		const firstEmptyIndex = state.selectedCompetitors.findIndex(
-			(value) => value === null,
-		);
-		if (firstEmptyIndex >= 0)
-			state.selectedCompetitors[firstEmptyIndex] = action.payload;
-
-		return state;
 	},
 });
